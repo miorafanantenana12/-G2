de5226a1b0172a50420e3195b8df1a8cff779727
Brunoespa <163377047+Brunoespa@users.noreply.github.com>
2024-03-25 06:18:03 +0300

    help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ Mocha | 🧋✨ ]";
+const doNotDelete = "💕❤️Bruno❤️💕";
 /** 
 * @author NTKhang
 * @author: do not delete it
