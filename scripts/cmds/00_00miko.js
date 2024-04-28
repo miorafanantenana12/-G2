5fc5e53fabca3c6cda5c8effb63e77922e8458e8
miorafanantenana12 <166400877+miorafanantenana12@users.noreply.github.com>
2024-04-28 21:58:03 +0300

    Update 00_00miko.js

--- scripts/cmds/00_00miko.js
+++ scripts/cmds/00_00miko.js
@@ -1,35 +1,37 @@
 const axios = require('axios');
 
-module.exports.config = {
-	name: "miko",
-	version: "1.0.0",
-	hasPermission: 0,
-	credits: "Cliff",//api by Miko
-	description: "cmd ai powered by blueai",
-	usePrefix: false,
-	commandCategory: "Educ",
-	usages: "blue [your content]",
-	cooldowns: 5
-};
+module.exports = {
+    config: {
+        name: "miko",
+        version: "1.0.0",
+        hasPermission: 0,
+        credits: "Cliff", //api by Miko
+        description: "cmd ai powered by blueai",
+        usePrefix: false,
+        commandCategory: "Educ",
+        usages: "blue [your content]",
+        cooldowns: 5,
+    },
 
-module.exports.run = async function ({ api, event, args }) {
-	const content = encodeURIComponent(args.join(" "));
+    onStart: async function ({ api, event, args }) {
+        const content = encodeURIComponent(args.join(" "));
 
-	if (!content) {
-		return api.sendMessage("🟢 Please  Provide your question first", event.threadID, event.messageID);
-	}
+        if (!content) {
+            return api.sendMessage("🟢 Please provide your question first", event.threadID, event.messageID);
+        }
 
-	api.sendMessage("🟡 Ai is typing  Please wait a seconds...", event.threadID, event.messageID); 
+        api.sendMessage("🟡 Ai is typing. Please wait a moment...", event.threadID, event.messageID);
 
-	const apiUrl = `https://bluerepoapislasttry.onrender.com/hercai?content=${content}`;
+        const apiUrl = `https://bluerepoapislasttry.onrender.com/hercai?content=${content}`;
 
-	try {
-		const response = await axios.get(apiUrl);
-		const reply = response.data.reply;
+        try {
+            const response = await axios.get(apiUrl);
+            const reply = response.data.reply;
 
-		api.sendMessage(reply, event.threadID, event.messageID);
-	} catch (error) {
-		console.error("Error fetching data:", error.message);
-		api.sendMessage("An error occurred while processing your request.", event.threadID);
-	}
+            api.sendMessage(reply, event.threadID, event.messageID);
+        } catch (error) {
+            console.error("Error fetching data:", error.message);
+            api.sendMessage("An error occurred while processing your request.", event.threadID);
+        }
+    }
 };
